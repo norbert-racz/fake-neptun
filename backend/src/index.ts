7aa05b7845f6549acb42c55648fe27a3bd651b3d
Racz Norbert <racznorbert766@gmail.com>
2024-04-14 02:47:49 +0200

    Created the backend server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,1 +1,8 @@
-console.log("Me work");
+import express from "express";
+
+const app = express();
+const port = 5000;
+
+app.listen(port, () => {
+    console.log('Server is listening on port ' + port.toString());
+});
