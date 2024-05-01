f31727df8ab4c926de92aff17cf288f137fdc9ce
Racz Norbert <racznorbert766@gmail.com>
2024-05-01 19:18:46 +0200

    Made register field required

--- frontend/src/app/register/register.component.ts
+++ frontend/src/app/register/register.component.ts
@@ -1,23 +1,42 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { routePaths } from '../app.routes';
 import { FormBuilder } from '@angular/forms';
+import FakeNeptunTexts from 'fake_neptun_common/build/fake_neptun_texts';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
 
+  userNameErrorMessage = FakeNeptunTexts.USERNAME_IS_REQUIRED_ERROR_MESSAGE;
+  passwordErrorMessage = FakeNeptunTexts.PASSWORD_IS_REQUIRED_ERROR_MESSAGE;
+  passwordAgainErrorMessage = FakeNeptunTexts.PASSWORD_AGAIN_IS_REQUIRED_ERROR_MESSAGE;
+
   register = this.formBuilder.group({
-    username: '',
-    password: '',
-    passwordAgain: ''
+    username: [
+      '',
+      Validators.required
+    ],
+    password: [
+      '',
+      Validators.required
+    ],
+    passwordAgain: [
+      '',
+      Validators.required
+    ]
   });
+
+  submitted = false;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -25,6 +44,9 @@ export class RegisterComponent {
   ){}
 
   public onSubmit(){
-    this.router.navigateByUrl(routePaths.REGISTRATION_STATUS);
+    this.submitted = true;
+    if(this.register.valid){
+      this.router.navigateByUrl(routePaths.REGISTRATION_STATUS);
+    }
   }
 }
